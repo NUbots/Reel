61d460c5f813f4625c4ca1da652f5301e5a9d905
Alex Biddulph <c3124185@uon.edu.au>
2017-12-12 16:57:21 +1100

    Adds root protobuf and libraries zlib and protobuf.

--- reel.py
+++ reel.py
@@ -1,11 +1,52 @@
 #!/usr/bin/env python3
 
+import os
+
 from reel import Reel
 
-r = Reel(gnu_mirror='http://gnu.uberglobalmirror.com')
+r = Reel() #gnu_mirror='http://gnu.uberglobalmirror.com')
 
-r.add_toolchain('nuc7i7bnh', triple='x86_64-linux-musl')
-r.add_toolchain('jetsontx2', triple='aarch64-linux-musl')
+r.add_library(
+    url='https://github.com/google/protobuf/releases/download/v3.5.0/protobuf-cpp-3.5.0.tar.gz',
+    name='protobuf',
+    configure_args={
+        '--with-zlib': True,
+        '--enable-static': True,
+        '--enable-shared': True
+    }
+)
+
+toolchains = [
+    r.add_toolchain('nuc7i7bnh', triple='x86_64-linux-musl'),
+    r.add_toolchain('jetsontx2', triple='aarch64-linux-musl')
+]
+
+for t in toolchains:
+    t.add_library(
+        name='zlib',
+        url='http://www.zlib.net/zlib-1.2.11.tar.gz',
+        configure_args={
+            '--host': None, # zlib configure doesn't understand host
+            '--build': None, # zlib configure doesn't understand build
+            '--static': True,
+            '--shared': True
+        }
+    )
+    t.add_library(
+        url='https://github.com/google/protobuf/releases/download/v3.5.0/protobuf-cpp-3.5.0.tar.gz',
+        name='protobuf',
+        env={
+            'CC_FOR_BUILD': '{} -static'.format(t.parent_toolchain.env['CC']),
+            'CXX_FOR_BUILD': '{} -static'.format(t.parent_toolchain.env['CXX'])
+        },
+        configure_args={
+            '--with-zlib': True,
+            '--with-protoc': os.path.join('{parent_prefix_dir}', 'bin', 'protoc'),
+            '--enable-static': True,
+            '--enable-shared': True,
+            '--verbose': True
+        }
+    )
 
 r.build()
 
