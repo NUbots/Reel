d0b60969242647a6998c99243f3d88ffb08ca18e
Alex Biddulph <c3124185@uon.edu.au>
2017-12-15 18:08:28 +1100

    Adds functionality for in source builds and non-root source builds.

--- reel/build/AutotoolsBuild.py
+++ reel/build/AutotoolsBuild.py
@@ -19,6 +19,7 @@ class AutotoolsBuild:
         }
 
         self.configure_args.update(build_args.get('configure_args', {}))
+        self.src_dir = build_args.get('src_dir', '.')
         self.build_postfix = build_args.get('build_postfix', '')
         self.build_args = build_args.get('build_args', [])
         self.install_args = build_args.get('install_args', [])
@@ -27,6 +28,9 @@ class AutotoolsBuild:
         self.build_targets = build_args.get('build_targets', ['all'])
         self.install_targets = build_args.get('install_targets', ['install'])
 
+        # Because asshats.
+        self.in_source_build = build_args.get('in_source_build', False)
+
         # Build our environment variables
         self.env = dict(os.environ)
 
@@ -34,7 +38,56 @@ class AutotoolsBuild:
         if 'env' in build_args:
             self.env.update(build_args['env'])
 
+    def autogen(self, **state):
+        # Work out our real full paths
+        src_path, base_src, logs_path, build_path, status_path = self.get_paths(state)
+
+        if os.path.isfile(os.path.join(src_path, 'autogen.sh')):
+            # Load the status file.
+            status = get_status(status_path)
+
+            if 'pre_configure' not in status or not status['pre_configure']:
+                # Make our build directory and log directory
+                os.makedirs(build_path, exist_ok=True)
+                os.makedirs(logs_path, exist_ok=True)
+
+                # Open a log file and run configure
+                with open(os.path.join(logs_path, '{}_pre_configure.log'.format(base_src)), 'w') as logfile:
+                    self.env.update({'NOCONFIGURE': '1'})
+                    cmd = os.path.abspath(os.path.join(src_path, 'autogen.sh'))
+                    print(indent(' $ {}'.format(cmd), 8))
+                    process = Popen(
+                        args=cmd,
+                        shell=True,
+                        cwd=os.path.abspath(src_path),
+                        env={k: v.format(**state)
+                             for k, v in self.env.items()},
+                        stdout=logfile,
+                        stderr=logfile
+                    )
+                    if process.wait() != 0:
+                        raise Exception('Failed to pre_configure')
+
+                    else:
+                        status = update_status(status_path, {'pre_configure': True})
+
+            else:
+                cprint(
+                    indent('Pre-configure step for {} complete... Skipping...'.format(base_src), 8),
+                    'yellow',
+                    attrs=['bold']
+                )
+
+        return {'build': build_path, 'logs': logs_path}
+
     def configure(self, **state):
+
+        # Work out our real full paths
+        src_path, base_src, logs_path, build_path, status_path = self.get_paths(state)
+
+        # Generate configure file if it does not already exist
+        if not os.path.isfile(os.path.join(src_path, self.src_dir, 'configure')):
+            self.autogen(**state)
 
         # Apply our state
         args = [
@@ -42,9 +95,6 @@ class AutotoolsBuild:
             for k, v in self.configure_args.items()
             if v is not None
         ]
-
-        # Work out our real full paths
-        src_path, base_src, logs_path, build_path, status_path = self.get_paths(state)
 
         # Load the status file.
         status = get_status(status_path)
@@ -54,9 +104,29 @@ class AutotoolsBuild:
             os.makedirs(build_path, exist_ok=True)
             os.makedirs(logs_path, exist_ok=True)
 
+            if self.in_source_build:
+                with open(os.path.join(logs_path, '{}_clone.log'.format(base_src)), 'w') as logfile:
+                    cmd = 'cp -rv {} {}'.format(
+                        os.path.abspath(os.path.join(src_path, self.src_dir, '*')), os.path.abspath(build_path)
+                    )
+                    print(indent(' $ {}'.format(cmd), 8))
+                    process = Popen(
+                        args=cmd,
+                        shell=True,
+                        cwd=os.path.abspath(build_path),
+                        env={k: v.format(**state)
+                             for k, v in self.env.items()},
+                        stdout=logfile,
+                        stderr=logfile
+                    )
+
             # Open a log file and run configure
             with open(os.path.join(logs_path, '{}_configure.log'.format(base_src)), 'w') as logfile:
-                cmd = '{} {}'.format(os.path.abspath(os.path.join(src_path, 'configure')), ' '.join(args))
+                cmd = '{} {}'.format(
+                    os.path.abspath(
+                        os.path.join(src_path if not self.in_source_build else build_path, self.src_dir, 'configure')
+                    ), ' '.join(args)
+                )
                 print(indent(' $ {}'.format(cmd), 8))
                 process = Popen(
                     args=cmd,
@@ -91,19 +161,11 @@ class AutotoolsBuild:
         # Otherwise run make for each of our targets
         for target in self.build_targets:
             if 'make_{}'.format(target) not in status or not status['make_{}'.format(target)]:
-                print(
-                    indent(
-                        ' $ {}'.format(
-                            ' '.join([
-                                'make', '-j{} {}'.format(
-                                    state['cpu_count'], ' '.join(a.format(**state) for a in self.build_args)
-                                ), target
-                            ])
-                        ), 8
-                    )
-                )
                 with open(os.path.join(logs_path, '{}_make_{}.log'.format(base_src, target)), 'w') as logfile:
-                    cmd = 'make -j{} {}'.format(state['cpu_count'], target)
+                    cmd = 'make -j{} {} {}'.format(
+                        state['cpu_count'], ' '.join(a.format(**state) for a in self.build_args), target
+                    )
+                    print(indent(' $ {}'.format(cmd), 8))
                     process = Popen(
                         args=cmd,
                         shell=True,
