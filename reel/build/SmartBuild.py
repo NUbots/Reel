a052d6423569994e03e8660f36fbdfe9d2c472e1
Alex Biddulph <c3124185@uon.edu.au>
2017-12-27 21:18:41 +1100

    Allows build tool to be forced.
    Accounts for libraries that provide multiple build systems.

--- reel/build/SmartBuild.py
+++ reel/build/SmartBuild.py
@@ -14,27 +14,39 @@ class SmartBuild:
         # Start without a downloader
         self.build_args = build_args
         self.src_dir = build_args.get('src_dir', '.')
+        self.use_tool = build_args.get('build_tool', None)
         self.build_tool = None
 
     def configure(self, **state):
 
         source = state['source']
 
-        # First check to see if we have a configure script
-        if os.path.isfile(os.path.join(source, self.src_dir, 'configure')) or os.path.isfile(os.path.join(
-                source, self.src_dir, 'autogen.sh')):
-            self.build_tool = AutotoolsBuild(**self.build_args)
+        if self.use_tool:
+            if self.use_tool == 'autotools':
+                self.build_tool = AutotoolsBuild(**self.build_args)
 
-        # Then check for CMakeLists.txt
-        elif os.path.isfile(os.path.join(source, self.src_dir, 'CMakeLists.txt')):
-            self.build_tool = CMakeBuild(**self.build_args)
+            elif self.use_tool == 'cmake':
+                self.build_tool = CMakeBuild(**self.build_args)
 
-        # Bjam
-        # Bazel
+            elif self.use_tool == 'make':
+                self.build_tool = MakeBuild(**self.build_args)
 
-        # Then check for a Makefile
-        elif os.path.isfile(os.path.join(source, self.src_dir, 'Makefile')):
-            self.build_tool = MakeBuild(**self.build_args)
+        else:
+            # First check to see if we have a configure script
+            if os.path.isfile(os.path.join(source, self.src_dir, 'configure')) or os.path.isfile(os.path.join(
+                    source, self.src_dir, 'autogen.sh')):
+                self.build_tool = AutotoolsBuild(**self.build_args)
+
+            # Then check for CMakeLists.txt
+            elif os.path.isfile(os.path.join(source, self.src_dir, 'CMakeLists.txt')):
+                self.build_tool = CMakeBuild(**self.build_args)
+
+            # Bjam
+            # Bazel
+
+            # Then check for a Makefile
+            elif os.path.isfile(os.path.join(source, self.src_dir, 'Makefile')):
+                self.build_tool = MakeBuild(**self.build_args)
 
         return self.build_tool.configure(**state)
 
