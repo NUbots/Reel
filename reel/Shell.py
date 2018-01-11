b6cffd6c4306039180ca6f8e03014d2b92abc22d
Alex Biddulph <c3124185@uon.edu.au>
2018-01-11 21:29:32 +1100

    Corrects log file location for shell phases.

--- reel/Shell.py
+++ reel/Shell.py
@@ -25,17 +25,21 @@ class Shell:
                 src_path = state['source']
                 base_src = os.path.basename(src_path)
                 status_path = os.path.join(state['status_dir'], '{}.json'.format(base_src))
+                logs_path = os.path.join(state['logs_dir'], base_src)
 
             else:
                 src_path = state['archive']
                 base_src = os.path.basename(src_path)
                 status_path = os.path.join(state['status_dir'], '{}.json'.format(base_src))
+                logs_path = os.path.join(state['logs_dir'], base_src)
 
             # Load the status file
             status = get_status(status_path)
 
             if phase not in status or not status[phase]:
-                with open(os.path.join(state['logs_dir'], '{}_{}.log'.format(base_src, phase)), 'w') as logfile:
+                os.makedirs(logs_path, exist_ok=True)
+
+                with open(os.path.join(logs_path, '{}_{}.log'.format(base_src, phase)), 'w') as logfile:
                     print(indent(' $ {}'.format(command.format(**state)), 8))
                     process = Popen(
                         args=command.format(**state),
