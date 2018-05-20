c3393921ad03e3f22d38628435cd6dce7bc46ac6
Alex Biddulph <c3124185@uon.edu.au>
2018-05-20 16:23:19 +1000

    Removes threaded version of ncurses
    The threaded version adds a t suffix to all of the libraries and nothing knows how to find the libraries when they are named like this

--- reel/__init__.py
+++ reel/__init__.py
@@ -110,7 +110,6 @@ class Reel:
                 '--with-termlib': True,
                 '--with-ticlib': True,
                 '--with-gpm': True,
-                '--with-pthread': True,
                 '--enable-sp-funcs': True,
                 '--enable-const': True,
                 '--enable-ext-colors': True,
