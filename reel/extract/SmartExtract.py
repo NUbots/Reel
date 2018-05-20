cfddd87b01ec52bd622edd3be5bee21e737437ed
Alex Biddulph <c3124185@uon.edu.au>
2018-05-20 16:09:13 +1000

    Adds ability to handle zip files

--- reel/extract/SmartExtract.py
+++ reel/extract/SmartExtract.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from .TarExtract import TarExtract
+from .ZipExtract import ZipExtract
 
 
 class SmartExtract:
@@ -13,8 +14,13 @@ class SmartExtract:
         # If we have an archive
         if 'archive' in state:
 
-            # For now assume tar file
-            return TarExtract(**self.build_args).extract(**state)
+            if state['archive'].endswith('.zip'):
+                return ZipExtract(**self.build_args).extract(**state)
+            elif state['archive'].endswith(('.tgz', '.tar.gz', '.txz', '.tar.xz', '.tbz ', '.tbz2 ', '.tar.bz2')):
+                return TarExtract(**self.build_args).extract(**state)
+            else:
+                # This isn't a known archive format, its probably a single file library
+                return {'source': state['archive']}
 
         else:
             raise Exception('No archive was found to extract')
